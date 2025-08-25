442c83d040d3ddb2c7f3bfd66abe811aefa58fa7
aliriodi <aliriodi@gmail.com>
2025-08-25 18:33:39 -0300

    cargando llaveros crearGFood

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,14 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   images: {
-    domains: ['res.cloudinary.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
