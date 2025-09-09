43204da46b0ff4b93e5d1351ab0e5ee311dd2e89
aliriodi <aliriodi@gmail.com>
2025-09-09 16:58:48 -0300

    asd

--- config/transporter.js
+++ config/transporter.js
@@ -5,7 +5,7 @@ export async function getGmailTransporter() {
   const oAuth2Client = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET
-  );
+  ); 
   oAuth2Client.setCredentials({ refresh_token: process.env.GOOGLE_REFRESH_TOKEN });
 
   const accessToken = await oAuth2Client.getAccessToken();
